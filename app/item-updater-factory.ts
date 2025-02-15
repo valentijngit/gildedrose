136561a9a2e32a78e04e56f820886364e35232c6
valentijngit <valentijnvanw@gmail.com>
2025-02-15 17:38:41 +0100

    more generic, problem with conjured items fixed, ran test with new snapshot

--- app/item-updater-factory.ts
+++ app/item-updater-factory.ts
@@ -6,18 +6,19 @@ import { BackstagePassesUpdater } from "./backstage-passes-updater";
 import { ConjuredItemUpdater } from "./conjured-item-updater";
 
 export class ItemUpdaterFactory {
+  private static readonly itemMap: Record<string, new () => ItemUpdater> = {
+    "Aged Brie": AgedBrieUpdater,
+    "Sulfuras": SulfurasUpdater,
+    "Backstage passes": BackstagePassesUpdater,
+    "Conjured": ConjuredItemUpdater
+  };
+
   static create(itemName: string): ItemUpdater {
-    switch(itemName) {
-      case 'Aged Brie':
-        return new AgedBrieUpdater();
-      case 'Sulfuras, Hand of Ragnaros':
-        return new SulfurasUpdater();
-      case 'Backstage passes to a TAFKAL80ETC concert':
-          return new BackstagePassesUpdater();
-      case 'Conjured item':
-          return new ConjuredItemUpdater();
-      default:
-        return new NormalItemUpdater();
+    for(const key in this.itemMap) {
+      if(itemName.includes(key)) {
+        return new this.itemMap[key]();
+      }
     }
+    return new NormalItemUpdater();
   }
 }
