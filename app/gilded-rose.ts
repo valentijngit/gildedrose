a5f3766bec3b47cb78fa0458de4dcf4770f25feb
valentijngit <valentijnvanw@gmail.com>
2025-02-15 20:54:53 +0100

    Business external not checked

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,6 +1,7 @@
 import { Item } from './item';
 import { ItemUpdaterFactory } from './item-updater-factory';
 
+//note to self: business expects this to be a daily but this code does depwned on jobs so shoukd chech diff in days last run at least
 export class GildedRose {
   items: Array<Item>;
 
